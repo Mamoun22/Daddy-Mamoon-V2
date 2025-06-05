ef1b055325d32e3d513e8ca5631778e978f27d94
Mamoun22 <mamoonkhadir96@gmail.com>
2025-06-05 09:05:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTA3MzI5ODczMjU0OTk1MTQ4OQ.GhJdOh.ikgK1ApMy9_25hfNXqQ6OB09Ox0y0Rf3txbpys",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  mongodbUri : "mongodb+srv://mamoonkhadir96:IGiVfU6THQVgBrEQ@daddymamoonv2.aswrqrh.mongodb.net/?retryWrites=true&w=majority&appName=DaddyMamoonV2",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
